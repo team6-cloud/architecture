21d2d196f8e8fa60b0fdada5a3bfe04d3e574596
joanpont <joan@glamsw.com>
2024-02-24 16:23:48 +0100

    Comment mongo repository

--- fase2/ECS/ecr_mongodb.tf
+++ fase2/ECS/ecr_mongodb.tf
@@ -1,7 +1,7 @@
-resource "aws_ecr_repository" "mongo" {
-  name                 = "mongo"
-  image_tag_mutability = "IMMUTABLE"
-  image_scanning_configuration {
-    scan_on_push = false
-  }
-}
+# resource "aws_ecr_repository" "mongo" {
+#   name                 = "mongo"
+#   image_tag_mutability = "IMMUTABLE"
+#   image_scanning_configuration {
+#     scan_on_push = false
+#   }
+# }
--- fase2/ECS/main.tf
+++ fase2/ECS/main.tf
@@ -1,3 +1,12 @@
+# role
+# data "aws_iam_role" "example_role" {
+# name = "AWSReservedSSO_AdministratorAccess_a88e813xf3c99999"
+# }
+
+# output "role_arn" {
+# value = data.aws_iam_role.example_role.arn
+# }
+
 # alb
 resource "aws_lb" "app-alb" {
   name                       = "app-alb"
