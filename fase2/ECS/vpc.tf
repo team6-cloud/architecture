d844ac4bcd0a7f3fe86fcfd5d015d4a1969acccb
joanpont <joan@glamsw.com>
2024-02-24 19:26:11 +0100

    More tests

--- fase2/ECS/vpc.tf
+++ fase2/ECS/vpc.tf
@@ -13,24 +13,17 @@ resource "aws_security_group" "alb" {
   name        = "${var.prefix}-alb-sg"
   description = "Application Load Balancer security group"
   vpc_id      = module.vpc.vpc_id
-  # ingress {
-  #   protocol    = "tcp"
-  #   from_port   = 3000
-  #   to_port     = 3000
-  #   cidr_blocks = ["0.0.0.0/0"]
-  # }
-
-  # ingress {
-  #   protocol    = "tcp"
-  #   from_port   = 3001
-  #   to_port     = 3001
-  #   cidr_blocks = ["0.0.0.0/0"]
-  # }
+  ingress {
+    protocol    = "tcp"
+    from_port   = 3000
+    to_port     = 3000
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   ingress {
-    protocol    = "-1"
-    from_port   = 0
-    to_port     = 0
+    protocol    = "tcp"
+    from_port   = 3001
+    to_port     = 3001
     cidr_blocks = ["0.0.0.0/0"]
   }
 
