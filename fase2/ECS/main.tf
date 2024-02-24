448d9ec87f75cf62f6422afee55c421ac86efc36
joanpont <joan@glamsw.com>
2024-02-24 17:05:29 +0100

    forgotten file when commit cloudwatch update

--- fase2/ECS/main.tf
+++ fase2/ECS/main.tf
@@ -1,12 +1,3 @@
-# role
-# data "aws_iam_role" "example_role" {
-# name = "AWSReservedSSO_AdministratorAccess_a88e813xf3c99999"
-# }
-
-# output "role_arn" {
-# value = data.aws_iam_role.example_role.arn
-# }
-
 # alb
 resource "aws_lb" "app-alb" {
   name                       = "app-alb"
